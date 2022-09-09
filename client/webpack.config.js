5f8144e4f41716d03536c9891f5f530060cff904
Kevin Reid <kevinnivek@me.com>
2022-09-08 20:56:42 -0400

    codemirror error fixed - thanks Anders

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -39,8 +39,17 @@ module.exports = () => {
               ]
             }
           }
-        }
+        },
+        // {
+      //     test: /\.css$/,
+      //     use: [
+      //         'style-loader',
+      //         { loader: 'css-loader', options: { importLoaders: 1 } },
+      //         'postcss-loader'
+      //     ]
+      // },
       ],
+      
   },
     plugins: [
       new HtmlWebpackPlugin({
